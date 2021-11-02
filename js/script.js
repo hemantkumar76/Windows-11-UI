3eb23fc3abada097781d7d3d0f898d9e6bec60f2
Pranay <pranayguptamain@gmail.com>
2021-11-02 21:31:11 +0530

    formatted the code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let taskbaricons = document.getElementsByClassName("taskbaricons")[0]
 let start = document.getElementsByClassName("start")[0]
 
-taskbaricons.addEventListener("click", ()=>{
-    if(start.style.bottom == "50px"){
+taskbaricons.addEventListener("click", () => {
+    if (start.style.bottom == "50px") {
         start.style.bottom = "-655px"
     }
-    else{
+    else {
         start.style.bottom = "50px"
     }
 })
